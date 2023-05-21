74137c08b81533e72fbf6741837bffd2447fe1e0
UJWAL KUMAR <89293303+ujwxl23@users.noreply.github.com>
2023-05-21 23:22:50 +0530

    Better css and html
    Html and CSS is implemented work left in JavaScript.

---
+++ public/index.js
@@ -0,0 +1,37 @@
+let sliderContainer = document.getElementById('sliderContainer');
+let slider = document.getElementById('slider');
+
+let cards = slider.getElementsByTagName('li');
+
+let sliderContainerWidth = sliderContainer.clientWidth;
+let elementsToShow = 5;
+if(document.body.clientWidth<1000){
+  elementsToShow =1;
+}
+
+
+let cardWidth = sliderContainerWidth/elementsToShow;
+
+slider.style.width = cards.length*cardWidth+'px';
+slider.style.transition='margin';
+slider.style.transitionDuration='1s';
+
+for (let index = 0; index < cards.length; index++) {
+  const element = cards[index];
+  element.style.width = cardWidth+'px';
+  
+}
+
+function next(){
+  if(+slider.style.marginLeft.slice(0,-2) != -cardWidth*(cards.length - elementsToShow)){
+    slider.style.marginLeft = ((+slider.style.marginLeft.slice(0,-2))-cardWidth)+'px';
+  }
+  
+ 
+    
+}
+function prev(){
+  if(+slider.style.marginLeft.slice(0,-2) != 0){
+    slider.style.marginLeft = ((+slider.style.marginLeft.slice(0,-2))+cardWidth)+'px';
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      zIndex: {
+        '-1':'-1',
+        '1' : '1',
+      },
+    },
+  },
+  plugins: [],
+}
+
